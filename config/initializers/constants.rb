9afcd7df9a40a2b90531cc508c274133aa955863
gsnarawat <gopalnarawat@gmail.com>
2013-11-18 15:47:44 +0530

    removed my app info

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
-APP_ID= '677101878981226' # please change!
-APP_SECRET= 'c9b4ca306506f5b86f9c2dc180bb03e5' # please change!
-SITE_URL = 'http://www.securemysocial.dev:4000'
+APP_ID= 'your app id here' # please change!
+APP_SECRET= 'your app secret here' # please change!
+SITE_URL = 'localhost:4000'
